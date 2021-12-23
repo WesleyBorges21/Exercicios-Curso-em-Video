2d8f56b61ed1988539e337d3299f851ded835e59
wesley <wesleybm191@gmail.com>
2021-12-23 13:56:54 -0300

    adiçao de ex dia 23/12

---
+++ ex012.py
@@ -0,0 +1,2 @@
+preço = float(input('Qual preço do produto? R$ '))
+print('O produto que custava R${:.2f}, na promoção com 5% de desconto vai custar R${:.2f}'.format(preço, (preço * 0.95)))
---
+++ ex014.py
@@ -0,0 +1,3 @@
+temp = float(input('Informe uma temperatura em ºC:'))
+fahr = ((temp * 9) / 5) + 32
+print('A temperatura de {:.1f}ºC corresponde a {:.1f}ºF!'.format(temp, fahr))
